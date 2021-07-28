539c4e75b4ab1dd9e95ab238b797bbf909f2deea
David-Salonga <87625948+David-Salonga@users.noreply.github.com>
2021-07-28 16:11:29 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-            // printer will print both color or black n white
+            // printer will print colored or black n white
             Printer print = new Printer();
             print.print("Print in black please");
             print.print(true);
